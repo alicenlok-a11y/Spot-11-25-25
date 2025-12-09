dae68842c2e1277aa0c55e9f721f9ab0e6911d8f
alicenlok-a11y <alicen.lok@gmail.com>
2025-12-09 03:24:15 -0800

    push

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
-const editbtn = document.querySelector(".profile__edit-btn");
-const editProfileModal = document.querySelector("#edi-profile-modal");
-const editbtnclose = document.querySelector(".modal__close-btn");
+const editBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editBtnClose = editProfileModal.querySelector(".modal__close-btn");
 
-const newPstBtn = document,querySelector(".profile__new-post-btn")
-const newPostModal = document.querySelector("#new-post-modal")
-const newPostCloseBtn = document.querySelector(".modal__close-btn")
+const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
-editbtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal__is-opened")
-}
+editBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal__is-opened");
+});
 
-editbtnclose.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal__is-opened")
-}
+editBtnClose.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal__is-opened");
+});
 
-newPstBtn.addEventListener("click", function () {
+newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal__is-opened");
-}
+});
 
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
-}
+});
