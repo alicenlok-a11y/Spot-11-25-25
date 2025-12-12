c7034a4064933d1021fa72784fcc351cc81a2ea2
alicenlok-a11y <alicen.lok@gmail.com>
2025-12-12 03:19:25 -0800

    push

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const editBtn = document.querySelector(".profile__edit-btn");
   const editProfileModal = document.querySelector("#edit-profile-modal");
   const editBtnClose = editProfileModal?.querySelector(".modal__close-btn");
+  const editProfileNameInput = editProfileModal.querySelector(#profile-name-input);
+  const editProfileDescriptionInput = editProfileModal.querySelector(#profile-description-input);
 
   // New Post Modal
   const newPostBtn = document.querySelector(".profile__new-post-btn");
   const newPostModal = document.querySelector("#new-post-modal");
   const newPostCloseBtn = newPostModal?.querySelector(".modal__close-btn");
 
+  const profileNameEl = document.querySelector(".profile__name");
+  const profileDescriptionEl = document.querySelector(".profile__description");
+
   // Only add event listeners if elements exist
   if (editBtn && editProfileModal && editBtnClose) {
     editBtn.addEventListener("click", function () {
+      editProfileNameInput.value = profileNameEl.textContent;
       editProfileModal.classList.add("modal__is-opened");
     });
 
